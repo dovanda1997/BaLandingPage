895f0639b4f4fccf1e5631a9e1734a7cbd60bd91
dovanda1997 <dovandaa976@gmail.com>
2023-12-01 15:34:43 +0700

    updateadd

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 $(document).ready(function () {
-  $(".autoplay").slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    arrows: false,
-  });
-});
-
-$(document).ready(function () {
   $(".multiple-items").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 1000,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          autoplaySpeed: 1000,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          autoplaySpeed: 1000,
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 });
 
@@ -24,6 +34,27 @@ function toggleContent(id) {
 }
 
 function toggleQues(element) {
-  const content = element.querySelector('.ques-hiden');
-  content.style.display = (content.style.display === 'none') ? 'block' : 'none';
+  const content = element.querySelector(".ques-hiden");
+  content.style.display = content.style.display === "none" ? "block" : "none";
 }
+$(document).ready(function () {
+  $(".autoplay").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 1000,
+          arrows: false,
+        },
+      },
+    ],
+  });
+});
